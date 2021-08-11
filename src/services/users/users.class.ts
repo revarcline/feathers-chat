90400b8b5cfccfd5fa66a58e6615870745c09b38
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-11 09:04:04 -0500

    adding gravatar to user

--- src/services/users/users.class.ts
+++ src/services/users/users.class.ts
@@ -1,5 +1,21 @@
+import crypto from 'crypto';
+import { Params } from '@feathersjs/feathers';
 import { Service, NedbServiceOptions } from 'feathers-nedb';
 import { Application } from '../../declarations';
+
+// Gravatar image service
+const gravatarUrl = 'https://s.gravatar.com/avatar';
+// size query (60 px)
+const query = 's=60';
+// returns gravatar image for email
+const getGravatar = (email: string) => {
+  // gravatar uses md5 hash of an all-lowercase email address to get image
+  const hash = crypto
+    .createHash('md5')
+    .update(email.toLowerCase())
+    .digest('hex');
+  return `${gravatarUrl}/${hash}?${query}`;
+};
 
 export class Users extends Service {
   //eslint-disable-next-line @typescript-eslint/no-unused-vars
